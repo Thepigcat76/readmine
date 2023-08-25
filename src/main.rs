eddb6c25eb3cdb0841796b80454b7cc9a9e73485
thepigcat <benpospo@gmail.com>
2023-08-25 23:03:23 +0200

    basic stuff in place

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::{io::{self, Write, Read}, fs::File};
+
 fn main() {
     println!("Hello, world!");
+    println!("Please enter the README file or root-directory");
+    print!(">> ");
+    let input = input();
+    println!("{}", open(&input));
+}
+
+fn input() -> String {
+    io::stdout().flush().expect("Failed to flush stdout");
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer).expect("Failed to read input");
+    buffer.trim().to_string()
+}
+
+fn open(path: &str) -> String {
+    let mut file = File::open(path).expect(format!("Failed to open file: {}", path).as_str());
+    let mut buffer = String::new();
+    file.read_to_string(&mut buffer).expect("Failed to read file");
+    buffer
 }
